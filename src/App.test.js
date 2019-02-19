c18588c51e819de39d273def3e9c1b864e0da31b
Yves Sacurae <yvessacurae@gmail.com>
2019-02-19 10:51:10 -0300

    refactor: Refactor code and create new components with the features.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme';
 import App from './App'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
-
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toEqual(true);
+});
 
 
